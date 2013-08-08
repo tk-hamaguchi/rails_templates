e3d2ecf2b21ff8042b20280ef6da2914e14e95c3
Takahiro HAMAGUCHI <tk.hamaguchi@gmail.com>
2013-08-09 00:34:01 +0900

    Added Comments.

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
 rails_templates
 ===============
 
-Templates for Rails
+Application templates for Rails4
+
+
+ 
+Usage
+--------
+
+```
+$ rails new {app_name} -T --skip-bundle -m {template_url} 
+```
+
+For example...
+
+```
+$ rails new my_app -T --skip-bundle -m https://raw.github.com/tk-hamaguchi/rails_templates/master/plain_template.rb
+```
+
+
+Type of templates
+--------
+
+|                     | plain_template   | metro_template   |
+| ------------------- |:----------------:|:----------------:|
+| Test Frameworks     | RSpec + Cucumber | RSpec + Cucumber |
+| Test Data Generator | FactoryGirl      | FactoryGirl      |
+
--- plain_template.rb
+++ plain_template.rb
@@ -1,15 +1,29 @@
-@heroku_flag = true if yes? "Would you like to use heroku?"
+#####
+##
+## Plain Template for Rails4
+##
+## @author @tk_hamaguchi
+## @examples Generate with template
+##   $ rails new my_app -T --skip-bundle -m https://github.com/tk-hamaguchi/rails_templates/blob/master/plain_template.rb
+##
+#
 
+
+## Create and initialize Git repository.
 git :init
 git add: '.'
 git commit: '-a -m "first commit."'
 
+
+## Update gem source.
 add_source 'http://production.s3.rubygems.org'
 gsub_file 'Gemfile', "source 'https://rubygems.org'", "#source 'https://rubygems.org'"
 gsub_file 'Gemfile', /# (gem 'therubyracer'.*)/, '\1'
 gsub_file 'Gemfile', /# (gem 'capistrano'.*)/, '\1'
 gsub_file 'Gemfile', /# (gem 'bcrypt-ruby'.*)/, '\1'
 
+
+## Add gem files.
 gem 'devise', '~> 3.0.0'
 gem 'devise-i18n'
 gem 'simple_form'
@@ -42,13 +56,15 @@ gem_group :production do
   gem 'unicorn'
 end
 
+
+## Execute bundle install
 run 'bundle install'
 
 
+## Configure rails locales.
 gsub_file 'config/application.rb', /^# require "rails\/test_unit\/railtie"$/, ''
 gsub_file 'config/application.rb', /^( +)# (config.time_zone = ).+$/, '\1\2"Tokyo"'
 gsub_file 'config/application.rb', /^( +)# (config.i18n.default_locale = ).+$/, '\1\2:ja'
-
 
 inside('config/locales') do
   run 'curl -O https://raw.github.com/svenfuchs/rails-i18n/master/rails/locale/en.yml -O https://raw.github.com/svenfuchs/rails-i18n/master/rails/locale/ja.yml'
@@ -65,53 +81,68 @@ ja:
 CODE
 
 
-
+## Update test frameworks to rspec and cucumber with FactoryGirl.
 generate 'rspec:install'
 gsub_file 'config/application.rb', /^  end\nend$/, "\n    config.generators do |g|\n      g.test_framework      :rspec\n      g.integration_tool    :rspec\n      g.fixture_replacement :factory_girl, dir:'spec/factories'\n    end\n  end\nend"
+
+file 'lib/tasks/rspec.rake', <<-CODE
+require 'rspec/core/rake_task'
+RSpec::Core::RakeTask.new(:spec)
+CODE
 
 generate 'cucumber:install'
 
 run 'guard init'
 
 
-generate 'rails_config:install'
-generate 'kaminari:config'
-generate 'kaminari:views', 'default'
-generate 'simple_form:install'
-generate 'devise:install', '-q'
-
-environment "config.action_mailer.default_url_options = { host: 'localhost:3000' }"
-
-
+## Generate home page.
 generate :controller, 'welcome index'
 gsub_file 'config/routes.rb', /# (root 'welcome#index')/, '\1'
 gsub_file 'config/routes.rb', /^ +get "welcome\/index"$/, ''
+
+
+## Configure rails_config
+generate 'rails_config:install'
+
+
+## Configure kaminari
+generate 'kaminari:config'
+generate 'kaminari:views', 'default'
+
+
+## Configure simple_form
+generate 'simple_form:install'
+
+
+## Configure Devise
+generate 'devise:install', '-q'
+
+environment "config.action_mailer.default_url_options = { host: 'localhost:3000' }"
 
 generate 'devise:views'
 generate :devise, 'User', 'name:string', 'deleted_at:datetime', 'lock_version:integer'
 
 insert_into_file "app/views/layouts/application.html.erb", "<%= render partial: 'devise/shared/links', locals: {resource_name: User, devise_mapping:Devise.mappings[:user]} %>\n", after: "<!--/.well -->\n"
 
+
+## Generate My page
 generate :controller, 'my', 'top'
 gsub_file 'config/routes.rb', /^ +get "my\/top"$/, '  get "my" => "my#top"'
-
 
 insert_into_file 'app/controllers/my_controller.rb', "  before_filter :authenticate_user!\n", after: "ApplicationController\n"
 
 insert_into_file 'app/controllers/welcome_controller.rb', "\n\n  private\n\n    def redirect_to_my_top\n      redirect_to my_path if user_signed_in?\n    end\n", after: "  end"
 insert_into_file 'app/controllers/welcome_controller.rb', "\n  before_filter :redirect_to_my_top\n\n", before: "  def index"
 
-file 'lib/tasks/rspec.rake', <<-CODE
-require 'rspec/core/rake_task'
 
-RSpec::Core::RakeTask.new(:spec)
-CODE
-
-
+## Create databases
 rake "db:create"
 
+
+## Configure capistrano
 run 'mkdir -p misc/capistrano'
 
 inside('misc/capistrano') do
   run 'capify .'
 end
+
